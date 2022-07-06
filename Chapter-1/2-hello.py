80667e3803a73d1a6293b5d7089441faab360ed6
Rhomeinel <rhomeinel@gmail.com>
2022-07-06 14:24:57 +0000

    Add code to file

---
+++ Chapter-1/2-hello.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+#This program will ask the user their name and will welcome them 1.
+print("Hello, " + input("Enter your name: "))
+
+
+#This program will ask the user their name and will welcome them 2.
+name = input("Enter your name: ")
+print("Hi, " + name)
+
+
+#This program will ask the user their name and will welcome them 3.
+username = input("Enter your name: ")
+print("Hey " "{}".format(username))
+
+
+#This program will ask the user their name and will welcome them 4.
+name = input("Enter your name: ")
+print("Welcome " +  (name))
+
+
+#This program will ask the user their name and will welcome them 5.
+username = input("Enter your name: ")
+print(f"Welcome Back  {username}")
+
+
+#This program will ask the user their name and will welcome them 6.
