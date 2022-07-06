122533c8aa85730c56006d212dcca80f08a0cbe0
Rhomeinel <rhomeinel@gmail.com>
2022-07-06 12:40:47 +0000

    Add code to file

---
+++ Chapter-1/1-mailing_address.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# This program will print the username and mailing address.
+
+print("Rhoda Nelson")
+print("Ghana Communication Technology University")
+print("Department of Information Technology")
+print("100, Tesano")
+print("GA-167-2979 PMB")
+print("Accra - Ghana")
